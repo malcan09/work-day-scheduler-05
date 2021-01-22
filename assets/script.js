fba410c1e2861511a4e816934ba36897360711de
Miguel Alcantar <malcantar185@gmail.com>
2021-01-21 22:12:12 -0800

    updating files

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+// listener 
+$(document).ready(function() {
+    
+    $(".saveBtn").on("click", function() {
+      
+      var value = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+  
+      // save in localStorage
+      localStorage.setItem(time, value);
+    });
+  
+    function updatedHour() {
+      // get current number 
+      var currentHour = moment().hours();
+  
+      // loop 
+      $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+        // past the time
+        if (blockHour < currentHour) {
+          $(this).addClass("past");
+        } 
+        else if (blockHour === currentHour) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        } 
+        else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+      });
+    }
+  
+    updatedHour();
+  
+    // ineterval to see if updated time needs change
+    let interval = setInterval(updatedHour, 15000);
+  
+    // load any saved data from localStorage
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  
+    // current day on page
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+  });
